fe5a65be2254a0fc164a19fd8815340103d98657
mostlypanda <sarthakmittal1461@gmail.com>
2020-07-03 00:22:42 +0530

    routes created

---
+++ controllers/userroutes.js
@@ -0,0 +1,17 @@
+const User=require('../models/user');
+
+exports.signup=function(req,res){
+
+};
+
+exports.login=function(req,res){
+
+};
+
+exports.logout=function(req,res){
+
+};
+
+exports.auth=function(req,res){
+
+};
--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,13 @@
 module.exports=(app)=>{
-    
+    const user=require('../controllers/userroutes');
+    const blog=require('../controllers/blogroute');
+
+    //user routes
+    app.get('/api/auth',user.auth);
+    app.get('/api/logout',user.logout);
+    app.post('/api/register',user.signup);
+    app.post('/api/login',user.login);
+
+    //blog routes
+
 }
