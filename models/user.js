adce57596281a64296b6ff23e0e9f12a491ddf3c
mostlypanda <sarthakmittal1461@gmail.com>
2020-07-03 14:35:06 +0530

    user methods defined

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose=require('mongoose');
 const jwt=require('jsonwebtoken');
 const bcrypt=require('bcrypt');
+const confiq=require('../config/config').get(process.env.NODE_ENV);
 const Salt=10;
 
 const userSchema=mongoose.Schema({
@@ -34,7 +35,57 @@ const userSchema=mongoose.Schema({
         type: String
     }
 });
+// to signup a user
+userSchema.pre('save',function(next){
+    var user=this;
+    
+    if(user.isModified('password')){
+        bcrypt.genSalt(salt,function(err,salt){
+            if(err)return next(err);
 
+            bcrypt.hash(user.password,salt,function(err,hash){
+                if(err) return next(err);
+                user.password=hash;
+                next();
+            })
+
+        })
+    }
+    else{
+        next();
+    }
+});
+
+//to login
+userSchema.methods.comparepassword=function(password,cb){
+    bcrypt.compare(password,this.password,function(err,isMatch){
+        if(err) return cb(next);
+        cb(nul,isMatch);
+    });
+}
+
+// find by token
+userSchema.statics.findByToken=function(token,cb){
+    var user=this;
+
+    jwt.verify(token,config.SECRET,function(err,decode){
+        user.findOne({"_id": decode, "token":token},function(err,user){
+            if(err) return cb(err);
+            cb(null,user);
+        })
+    })
+};
+
+//delete token
+
+userSchema.methods.deleteToken=function(token,cb){
+    var user=this;
+
+    user.update({$unset : {token :1}},function(err,user){
+        if(err) return cb(err);
+        cb(null,user);
+    })
+}
 
 
 module.exports=mongoose.model('User',userSchema);
